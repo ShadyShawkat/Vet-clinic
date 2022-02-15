d87cc185896ebfa8e1ae54ef4a85c41e6344662a
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-15 19:44:57 +0200

    Insert Charmander

--- data.sql
+++ data.sql
@@ -6,3 +6,7 @@ VALUES
 	(2, 'Gabumon', '2018-11-15', 2, true, 8),
 	(3, 'Pikachu', '2021-01-07', 1, false, 15.04), 
 	(4, 'Devimon', '2017-05-12', 5, true, 11);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES 
+  (5, 'Charmander', '2020-02-08', 0, false, -11)
