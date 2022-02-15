73fa52fa18057d598e9afedb90aec8fa384a20b9
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-15 19:50:35 +0200

    Insert Angemon

--- data.sql
+++ data.sql
@@ -11,4 +11,5 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 VALUES 
   (5, 'Charmander', '2020-02-08', 0, false, -11),
 	(6, 'Plantmon', '2022-11-15', 2, true, -5.7),
-	(7, 'Squirtle', '1993-04-02', 3, false, -12.13)
+	(7, 'Squirtle', '1993-04-02', 3, false, -12.13),
+  (8, 'Angemon', '2005-06-12', 1, true, -45)
