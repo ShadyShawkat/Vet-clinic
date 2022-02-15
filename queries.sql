049ebe89cf79c274dc607d9efebee941d79ca07a
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-15 20:42:36 +0200

    Select average weight of all animals

--- queries.sql
+++ queries.sql
@@ -38,14 +38,12 @@ WHERE weight_kg
 BETWEEN 10.4 AND 17.3
 
 -- A transaction update the animals table by setting the species column to unspecified. Then roll back the change.
-
 BEGIN;
   UPDATE animals
   SET species = 'unspecified';
 ROLLBACK;
 
 -- A transaction that updates animals species according to their names.
-
 BEGIN;
   UPDATE animals
   SET species = 'digimon'
@@ -57,13 +55,11 @@ BEGIN;
 COMMIT;
 
 -- A transaction that deletes all records. Then roll back the change.
-
 BEGIN;
   DELETE FROM animals;
 ROLLBACK;
 
 -- A transaction that updates weigths to be positive
-
 BEGIN;
 	DELETE FROM animals
 	WHERE date_of_birth > '2022-01-01';
@@ -79,12 +75,21 @@ BEGIN;
 COMMIT;
 
 -- How many animals are there?
-
 SELECT COUNT(*) AS "Number of animals"
 FROM animals
 
 -- How many animals have never tried to escape?
-
 SELECT COUNT(*) AS "Number of animals that never tried to escape"
 FROM animals
 WHERE escape_attempts = 0
+
+-- What is the average weight of animals?
+SELECT CONCAT(AVG(weight_kg), ' kg') AS "Average weight of animals"
+FROM animals
+
+-- Who escapes the most, neutered or not neutered animals?
+
+-- What is the minimum and maximum weight of each type of animal?
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
