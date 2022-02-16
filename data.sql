b4403dbf9ea12cfa5a9035fa06ad1c0aa71f6a21
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-16 19:42:49 +0200

    Insert Pokemon species into species table

--- data.sql
+++ data.sql
@@ -25,3 +25,8 @@ INSERT INTO owners (full_name, age)
     ('Melody Pond', 77),
     ('Dean Winchester', 14),
     ('Jodie Whittaker', 38);
+
+-- Insert data into species table
+INSERT INTO species (name)  
+  VALUES 
+    ('Pokemon'),
