5eb687b9fc876573490c6fb84520b4155fe1e9b5
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-17 18:36:53 +0200

    Add date_of_visit column to visits table

--- schema.sql
+++ schema.sql
@@ -65,6 +65,7 @@ CREATE TABLE specializations (
 CREATE TABLE visits (
 	animal_id INT NOT NULL,
 	vet_id INT NOT NULL,
+	date_of_visit DATE,
 	FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	PRIMARY KEY (animal_id, vet_id)
