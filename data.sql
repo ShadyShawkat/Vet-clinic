a40ef5c0237f3e862115e80ab017677a894a0d7f
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-17 18:25:11 +0200

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -61,3 +61,11 @@ UPDATE animals
 UPDATE animals
   SET owner_id = 5
   WHERE name in ('Angemon', 'Boarmon');
+
+-- Insert data into vets table
+INSERT INTO vets (name, age, date_of_graduation)  
+  VALUES 
+    ('William Tatcher', 45, '2000-04-23'),
+    ('Maisy Smith', 26, '2019-01-17'),
+    ('Stephanie Mendez', 64, '1981-05-04'),
+    ('Jack Harkness', 38, '2008-06-08')
